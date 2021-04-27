9e389599cf3fbb14e863d175547085db7020a286
Alexandre PALPANT <alexandre.palpant@gmail.com>
2021-04-27 16:01:36 +0200

    Partie Serveur Web ok

--- Sandbox/TP_linux/provision_server_web.sh
+++ Sandbox/TP_linux/provision_server_web.sh
@@ -3,18 +3,12 @@
 apt install -y apache2
 apt install ufw
 
-ufw allow 80
-ufw allow 443
+ufw --force enable 
 
-mkdir /var/www/tp-linux
-mv /home/vagrant/tp-linux/index.html /var/www/tp-linux/
+ufw allow ssh
+ufw allow http
+ufw allow https
+
+mv /home/vagrant/share/index.html /var/www/html/
 cd /var/www
-chown -R www-data:www-data tp-linux/
-
-sed "s/#ServerName www.example.com/ServerName tp-linux/;s/html/tp-linux/" \
-/etc/apache2/sites-available/000-default.conf > /etc/apache2/sites-available/tp-linux.conf
-
-a2ensite tp-linux
-systemctl reload apache2
-
-echo "127.0.0.1 tp-linux" >> /etc/hosts
+chown -R www-data:www-data html/

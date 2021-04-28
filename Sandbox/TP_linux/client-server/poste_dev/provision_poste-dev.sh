9d3181461673ccb69d723b19023abac8e1eb9141
Alexandre PALPANT <alexandre.palpant@gmail.com>
2021-04-28 15:30:03 +0200

    Client

--- Sandbox/TP_linux/client-server/poste_dev/provision_poste-dev.sh
+++ Sandbox/TP_linux/client-server/poste_dev/provision_poste-dev.sh
@@ -1,7 +1,34 @@
 #!/bin/sh
 
-echo "Installation reussie"
+# On installe l'utilitaire sshpass
+apt install sshpass
 
-ssh-keygen -q -t rsa -N '' -f ~/.ssh/id_rsa <<<y 2>&1 >/dev/null
+# On genere une cle avec une passphrase a vide
+ssh-keygen -q -t rsa -N '' -f ~/.ssh/id_rsa
 
-ssh-copy-id -i ~/.ssh/id_rsa.pub userjob@192.168.0.42
+# On copie la cle sur le serveur avec le compte userjob
+sshpass -p userjob ssh-copy-id -i ~/.ssh/id_rsa.pub -o StrictHostKeyChecking=no userjob@192.168.1.42
+
+# Installation des logiciels demandes
+apt install -y python3 python-pip python3-dev git 
+
+# Debut installation de Visual Studio Code
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
+sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+rm -f packages.microsoft.gpg
+
+apt install -y apt-transport-https
+apt update
+apt install -y code
+# Fin installation de Visual Studio Code
+
+# Installation de vagrant
+apt install -y vagrant
+
+git clone https://github.com/vanessakovalsky/example-python.git
+
+python3 example-python/vagrant/main.py
+# TODO expect
+# Bien joué votre VM avec python fonctionne
+

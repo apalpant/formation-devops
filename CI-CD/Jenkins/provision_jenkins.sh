21f72167c1f2062888de0592fcd15a2be3421825
Alexandre PALPANT <alexandre.palpant@gmail.com>
2021-05-19 12:00:32 +0200

    Docker

--- CI-CD/Jenkins/provision_jenkins.sh
+++ CI-CD/Jenkins/provision_jenkins.sh
@@ -56,3 +56,35 @@ sed "s/PasswordAuthentication no/PasswordAuthentication yes/" \
 
 # On restart le service
 systemctl restart sshd
+
+apt install git
+
+# install python
+apt-get install python3 python3-dev python3-pip -q -y
+apt remove -y python 
+
+cp /usr/bin/python3 /usr/bin/python
+
+apt install -y openjdk-8-jdk
+apt install -y unzip
+
+## Récupération de la dernière version de GRADLE
+
+VERSION=7.0
+wget https://downloads.gradle-dn.com/distributions/gradle-${VERSION}-bin.zip -P /tmp
+
+unzip -d /opt/gradle /tmp/gradle-${VERSION}-bin.zip
+
+# Faire pointer le lien vers la dernière version de gradle
+
+ln -s /opt/gradle/gradle-${VERSION} /opt/gradle/latest
+
+# Ajout de gradle au PATH
+
+touch /etc/profile.d/gradle.sh
+
+echo "export PATH=/opt/gradle/latest/bin:${PATH}" > /etc/profile.d/gradle.sh
+
+chmod +x /etc/profile.d/gradle.sh
+
+source /etc/profile.d/gradle.sh
